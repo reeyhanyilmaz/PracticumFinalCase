f0e717671af67b0146805af5836919834b230f2a
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-09-25 10:54:05 +0300

    seperate sytyle files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
-import '../styles/globals.scss'
+import '../styles/globals.scss';
+import "../styles/banner.scss";
+import "../styles/modals.scss";
+
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
