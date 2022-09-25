44d903a7229f1fd10630a5c3678ac27490027c65
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-09-25 10:55:13 +0300

    add Header comp (for modals)

---
+++ src/features/modal/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="modalHeaderDiv">
+      <p className="p1">Modal Card Generator</p>
+      <p className="p2">
+        Measure your return on email marketing efforts easier and faster by
+        using thebest online tools. Popupsmart is ready to help you build an
+        efficient email list!
+      </p>
+    </div>
+  );
+}
+
+export default Header;
