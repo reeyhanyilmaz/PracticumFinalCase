66ec8d4b37d879ed459873ef5680e33ef0d7a326
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-09-28 23:22:01 +0300

    add modal data

---
+++ src/data/modal.ts
@@ -0,0 +1,70 @@
+import { IModalGridCard } from "../types/modal";
+
+export const ModalGridCardsData = <IModalGridCard[]>[
+    {
+        id: 1,
+        name: "Security Code Modal",
+        image: { url: "/assets/modal/modal1.svg", width: 170, height: 156 },
+    },
+    {
+        id: 2,
+        name: "Security Code Modal",
+        image: { url: "/assets/modal/modal1.svg", width: 170, height: 156 },
+    },
+    {
+        id: 3,
+        name: "Security Code Modal",
+        image: { url: "/assets/modal/modal1.svg", width: 170, height: 156 },
+    },
+    {
+        id: 4,
+        name: "Security Code Modal",
+        image: { url: "/assets/modal/modal1.svg", width: 170, height: 156 },
+    },
+    {
+        id: 5,
+        name: "Security Code Modal",
+        image: { url: "/assets/modal/modal1.svg", width: 170, height: 156 },
+    },
+    {
+        id: 6,
+        name: "Security Code Modal",
+        image: { url: "/assets/modal/modal1.svg", width: 170, height: 156 },
+    },
+    {
+        id: 7,
+        name: "Security Code Modal",
+        image: { url: "/assets/modal/modal1.svg", width: 170, height: 156 },
+    },
+    {
+        id: 8,
+        name: "Security Code Modal",
+        image: { url: "/assets/modal/modal1.svg", width: 170, height: 156 },
+    },
+    {
+        id: 9,
+        name: "Security Code Modal",
+        image: { url: "/assets/modal/modal1.svg", width: 170, height: 156 },
+    },
+    {
+        id: 10,
+        name: "Security Code Modal",
+        image: { url: "/assets/modal/modal1.svg", width: 170, height: 156 },
+    },
+
+    {
+        id: 11,
+        name: "Security Code Modal",
+        image: { url: "/assets/modal/modal1.svg", width: 170, height: 156 },
+    },
+    {
+        id: 12,
+        name: "Security Code Modal",
+        image: { url: "/assets/modal/modal1.svg", width: 170, height: 156 },
+    },
+    {
+        id: 13,
+        name: "Security Code Modal",
+        image: { url: "/assets/modal/modal1.svg", width: 170, height: 156 },
+    },
+];
