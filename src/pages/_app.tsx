2afebcb232c366f487f861e23e9b5236f71fc1e7
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-09-28 22:27:54 +0300

    adds redux provider in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
-import '../styles/globals.scss';
+import "../styles/globals.scss";
 import "../styles/banner.scss";
 import "../styles/modals.scss";
-import "../styles/bannerImage.scss"
+import "../styles/bannerImage.scss";
 
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { store } from "../redux/store";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
