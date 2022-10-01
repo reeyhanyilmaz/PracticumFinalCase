9dc9923b1eb23e3b3a8e2a5e4b37ad06109d53b8
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-10-01 16:33:08 +0300

    add color and size state and setState

--- src/redux/modalSlice.ts
+++ src/redux/modalSlice.ts
@@ -6,6 +6,8 @@ import {IModalState } from '../types/modal';
 
 const initialState: IModalState = {
     selectedModal: 0,
+    selectedModalSize: 2,
+    selectedColor: "#F37C34",
     fileUrl: "",
     loading: false,
 }
@@ -17,21 +19,29 @@ export const modalSlice = createSlice({
         setSelectedModal: (state, action: PayloadAction<number>) => {
             state.selectedModal = action.payload;       
         },
+        setSelectedModalSize: (state, action: PayloadAction<number>) => {
+            state.selectedModalSize = action.payload;       
+        },
         setFileUrl: (state, action: PayloadAction<string>) => {
             state.fileUrl = action.payload;
             console.log('action.payload', action.payload)
         },
         setLoading: (state, action: PayloadAction<boolean>) => {
             state.loading = action.payload;
+        },
+        setSelectedColor: (state, action: PayloadAction<string>) => {
+            state.selectedColor = action.payload;
         }
     },
 })
 
 // Reducers
-export const { setSelectedModal, setFileUrl, setLoading } = modalSlice.actions
+export const { setSelectedModal, setSelectedModalSize, setFileUrl, setLoading,setSelectedColor } = modalSlice.actions
 
 // Custom Hooks
 export const useSelectedModal = () => useAppSelector((s: RootState) => s.modal.selectedModal);
+export const useSelectedModalSize = () => useAppSelector((s:RootState) => s.modal.selectedModalSize);
+export const useSelectedColor = () => useAppSelector((s:RootState) => s.modal.selectedColor);
 export const useFileUrl = () => useAppSelector((s: RootState) => s.modal.fileUrl);
 export const useLoading = () => useAppSelector((s:RootState) => s.modal.loading);
 
