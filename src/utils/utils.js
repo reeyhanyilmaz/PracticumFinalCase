0f68f13f51640874f6a0b45ad0f3792cc82b7822
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-10-03 23:55:06 +0300

    add color picker utils

---
+++ src/utils/utils.js
@@ -0,0 +1,16 @@
+export const getTailWindBg = (color) => {
+  switch (color) {
+    case "#000000":
+      return "bg-[#000000]";
+    case "#F37C34":
+      return "bg-[#F37C34]";
+    case "#777777":
+      return "bg-[#777777]";
+    case "#DDDDDD":
+      return "bg-[#DDDDDD]";
+    case "#FFFFFF":
+      return "bg-[#FFFFFF]";
+    default:
+      return "bg-[#7D4AEA]";
+  }
+};
