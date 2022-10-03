fc06edaa82b241f05812632ac451aacb3cb33b20
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-10-03 14:10:46 +0300

    add selected, color, size func.

--- src/features/modal/modals/SecurityCodeModal.tsx
+++ src/features/modal/modals/SecurityCodeModal.tsx
@@ -4,17 +4,52 @@ import Image from "next/image";
 import ModalInput from "../../../components/ModalInput";
 import ModalButton from "../../../components/ModalButton";
 
+//redux
+import {
+  useFileUrl,
+  useSelectedColor,
+  useSelectedModal,
+  useSelectedModalSize,
+} from "../../../redux/modalSlice";
+
 function SecurityCodeModal() {
+  const fileUrl = useFileUrl();
+  const size = useSelectedModalSize();
+  const selectedModal = useSelectedModal();
+  const color = useSelectedColor();
+
   return (
-    <div className="generalModalStyle flex-col w-[480px] h-[446px] font-[Inter]">
+    <div
+      className={`generalModalStyle p-[20px] flex-col w-[${size * 240}] h-[${
+        size * 223
+      }] font-[Inter]`}
+      style={{height: size === 1 ? "223px" :  size === 2 ? "446px" : "668px", width: size === 1 ? "240px" :  size === 2 ? "480px" : "720px"}}
+
+    >
       <ModalCloseButton />
-      <div className="generalPosition bg-purple mb-[29px] h-[90px] w-[90px] rounded-[50%]">
-        <Image src="/assets/modal/securityCodeLogoContent.svg" width={36} height={44} />
+      <div
+        className={`generalPosition bg-primaryColor  mb-[29px] rounded-[50%]`}
+        style={{height: size === 1 ? "45px" :  size === 2 ? "90px" : "135px", width: size === 1 ? "45px" :  size === 2 ? "90px" : "135px"}}
+      >
+        <Image
+          src="/assets/modal/securityCodeLogoContent.svg"
+          width={size * 18}
+          height={size * 22}
+        />
       </div>
-      <p className="w-[209px] h-[36px] mb-[20px] font-bold text-2xl ">
+      <p
+        className={`mb-[20px] text-${
+          size === 1 ? "xl" : size === 2 ? "2xl" : "3xl"
+        } font-bold`}
+        style={{ fontSize: size === 3 ? "35px" : "" }}
+      >
         Security Code
       </p>
-      <p className="mb-[41.5px] text-xl font-normal">
+      <p
+        className={`mb-[41.5px]  text-${
+          size === 1 ? "lg" : size === 2 ? "xl" : "2xl"
+        } font-normal`}
+      >
         This code expires in 24 hours
       </p>
       <ModalInput placeholder="Code" />
@@ -28,9 +63,9 @@ function SecurityCodeModal() {
         <ModalButton
           title="Continue"
           width={168}
-          border="none"
-          bg="#7D4AEA"
-          color="#fff"
+          border={color === "#FFFFFF" ? "1px solid #D2DAE3" : "none"}
+          color={color === "#FFFFFF" ? "#000000" : "#fff"}
+          bg={color}
         />
       </div>
     </div>
